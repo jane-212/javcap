8e1ef9c2e3b4d04448fa2c621fdc807405e6af1a
jxt <jjt1525@outlook.com>
2025-02-08 00:27:33 +0800

    update

--- crates/app/build.rs
+++ crates/app/build.rs
@@ -4,14 +4,16 @@ fn main() {
     #[cfg(debug_assertions)]
     println!("cargo:rustc-env=VERSION=DEBUG");
 
-    let out = Command::new("git")
+    let git_hash = Command::new("git")
         .arg("rev-parse")
         .arg("--short")
         .arg("HEAD")
         .output()
-        .expect("execute git failed")
-        .stdout;
-    let git_hash = String::from_utf8(out).expect("parse output to string failed");
+        .map(|output| String::from_utf8(output.stdout))
+        .expect("get git hash failed")
+        .expect("get git hash failed")
+        .trim()
+        .to_string();
 
     println!("cargo:rustc-env=HASH={git_hash}");
 }
