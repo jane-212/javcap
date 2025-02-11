2222a9bec47840e3a5e33cddcbf40c6d12be4f1c
jane <jjt1525@outlook.com>
2025-02-11 19:56:15 +0800

    fix airav (#59)
    * fix airav
    
    * typos

--- crates/spider/src/airav.rs
+++ crates/spider/src/airav.rs
@@ -14,9 +14,10 @@ use super::{select, Finder};
 const HOST: &str = app::url::AIRAV;
 
 select!(
-    home_title: "body > div:nth-child(4) > div > div.row.row-cols-2.row-cols-lg-4.g-2.mt-0 > div:nth-child(1) > div > div.oneVideo-body > h5"
-    home_fanart: "body > div:nth-child(4) > div > div.row.row-cols-2.row-cols-lg-4.g-2.mt-0 > div:nth-child(1) > div > div.oneVideo-top > a > img"
-    home_url: "body > div:nth-child(4) > div > div.row.row-cols-2.row-cols-lg-4.g-2.mt-0 > div:nth-child(1) > div > div.oneVideo-top > a"
+    home_item: "body > div:nth-child(4) > div > div.row.row-cols-2.row-cols-lg-4.g-2.mt-0 > div"
+    home_title: "div > div.oneVideo-body > h5"
+    home_fanart: "div > div.oneVideo-top > a > img"
+    home_url: "div > div.oneVideo-top > a"
     detail_date: "body > div:nth-child(4) > div.container > div > div.col-lg-9.col-12.pt-3 > div.video-item > div.me-4"
     detail_plot: "body > div:nth-child(4) > div.container > div > div.col-lg-9.col-12.pt-3 > div.video-info > p"
     detail_name: "body > div:nth-child(4) > div.container > div > div.col-lg-9.col-12.pt-3 > div.video-info > div > ul > li"
@@ -112,23 +113,36 @@ impl Airav {
             .await?;
         let html = Html::parse_document(&text);
 
-        if let Some(title) = html
-            .select(&self.selectors.home_title)
-            .next()
-            .map(|node| node.text().collect())
-        {
-            nfo.set_title(title);
+        let mut url = None;
+        let mut fanart = None;
+        let name = key.to_string();
+        for item in html.select(&self.selectors.home_item) {
+            let Some(title) = item
+                .select(&self.selectors.home_title)
+                .next()
+                .map(|node| node.text().collect::<String>())
+            else {
+                continue;
+            };
+            if !title.contains(&name) || title.contains("克破") {
+                continue;
+            }
+            nfo.set_title(title.trim_start_matches(&name).trim().to_string());
+
+            fanart = item
+                .select(&self.selectors.home_fanart)
+                .next()
+                .and_then(|node| node.attr("src").map(String::from));
+
+            url = item
+                .select(&self.selectors.home_url)
+                .next()
+                .and_then(|node| node.attr("href").map(|href| format!("{HOST}{href}")));
+
+            if url.is_some() && fanart.is_some() {
+                break;
+            }
         }
-
-        let fanart = html
-            .select(&self.selectors.home_fanart)
-            .next()
-            .and_then(|node| node.attr("src").map(String::from));
-
-        let url = html
-            .select(&self.selectors.home_url)
-            .next()
-            .and_then(|node| node.attr("href").map(|href| format!("{HOST}{href}")));
 
         Ok((url, fanart))
     }
@@ -231,11 +245,21 @@ mod tests {
                     .country(Country::Japan)
                     .mpaa(Mpaa::NC17)
                     .build();
-                nfo.set_title("STARS-804 馬賽克破壞版 STARS-804 神木麗".to_string())
-                    .set_plot("馬賽克破壞版 STARS-804 神木麗".to_string())
-                    .set_premiered("2023-04-11".to_string());
+                nfo.set_title("隨著本能絡合的極上內衣與精油4本番 神木麗".to_string())
+                    .set_plot("G罩杯的身材，搭配高級內衣和按摩油更能突顯出其絕佳的比例。神木麗進入飯店後懇求般進行濃厚的性愛。綑縛美麗四肢，持續玩弄身軀到絕頂後懇求插入，喘息聲無法壓抑響徹於房間…".to_string())
+                    .set_studio("SOD".to_string())
+                    .set_premiered("2023-04-06".to_string());
                 let actors = ["神木麗"];
-                let genres = ["無碼", "馬賽克破壞版"];
+                let genres = [
+                    "巨乳",
+                    "720p",
+                    "HD高畫質",
+                    "ローション・オイル",
+                    "AV女優片",
+                    "乳交",
+                    "中文",
+                    "性感內衣",
+                ];
 
                 for actor in actors {
                     nfo.actors_mut().insert(actor.to_string());
@@ -251,7 +275,7 @@ mod tests {
                     .country(Country::Japan)
                     .mpaa(Mpaa::NC17)
                     .build();
-                nfo.set_title("IPX-443 禁欲一個月後與朋友男友瘋狂做愛 明里紬 合計10回密着性交".to_string())
+                nfo.set_title("禁欲一個月後與朋友男友瘋狂做愛 明里紬 合計10回密着性交".to_string())
                     .set_plot("雖然我有老婆，但是看到了老婆朋友明里紬誘惑後就心癢癢。知道老婆要不在家開始，就禁慾一個月，只為了與明里紬從早到晚瘋狂做愛…".to_string())
                     .set_studio("IDEA POCKET".to_string())
                     .set_premiered("2020-02-13".to_string());
@@ -280,8 +304,7 @@ mod tests {
                     .mpaa(Mpaa::NC17)
                     .build();
                 nfo.set_title(
-                    "FC2-PPV-3061625 人生初拍攝。中出。年度最美少女被蒙面男子精子玷污的那一刻！"
-                        .to_string(),
+                    "人生初拍攝。中出。年度最美少女被蒙面男子精子玷污的那一刻！".to_string(),
                 )
                 .set_plot("本站獨家FC2素人影片，千萬別錯過!!".to_string())
                 .set_studio("FC2高清版".to_string())
