ebedf1953184ae6eb5a3e0444b954424a8b37402
jxt <jjt1525@outlook.com>
2025-02-18 01:45:38 +0800

    添加porny支持

--- crates/spider/src/porny.rs
+++ crates/spider/src/porny.rs
@@ -1,20 +1,32 @@
 use std::fmt::{self, Display};
 use std::time::Duration;
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use async_trait::async_trait;
 use bon::bon;
 use http_client::Client;
-use nfo::Nfo;
+use log::info;
+use nfo::{Country, Mpaa, Nfo};
+use scraper::Html;
 use video::VideoType;
 
-use super::Finder;
+use super::{select, Finder};
 
 const HOST: &str = "https://91porny.com";
+
+select!(
+    item: "#main > div.container-fluid.px-0 > div:nth-child(3) > div"
+    title: "div > a.title.text-sub-title.mt-2.mb-1"
+    author: "div > small > div:nth-child(1) > a"
+    date: "div > small > div:nth-child(2)"
+    runtime: "div > a.display.d-block > small"
+    fanart: "div > a.display.d-block > div.img"
+);
 
 pub struct Porny {
     base_url: String,
     client: Client,
+    selectors: Selectors,
 }
 
 #[bon]
@@ -31,12 +43,17 @@ impl Porny {
             .maybe_proxy(proxy)
             .build()
             .with_context(|| "build http client")?;
+        let selectors = Selectors::new().with_context(|| "build selectors")?;
         let base_url = match base_url {
             Some(url) => url,
             None => String::from(HOST),
         };
 
-        let porny = Porny { base_url, client };
+        let porny = Porny {
+            base_url,
+            selectors,
+            client,
+        };
         Ok(porny)
     }
 }
@@ -58,6 +75,118 @@ impl Finder for Porny {
     }
 
     async fn find(&self, key: &VideoType) -> Result<Nfo> {
-        todo!()
+        let mut nfo = Nfo::builder()
+            .id(key)
+            .country(Country::China)
+            .mpaa(Mpaa::NC17)
+            .build();
+
+        let fanart = self.search(key, &mut nfo).await?;
+        let fanart = self
+            .client
+            .wait()
+            .await
+            .get(fanart)
+            .send()
+            .await?
+            .bytes()
+            .await?
+            .to_vec();
+        nfo.set_fanart(fanart);
+
+        info!("{nfo:?}");
+        Ok(nfo)
+    }
+}
+
+impl Porny {
+    async fn search(&self, key: &VideoType, nfo: &mut Nfo) -> Result<String> {
+        let name = key.to_string();
+        let url = format!("{}/search", self.base_url);
+        let text = self
+            .client
+            .wait()
+            .await
+            .get(url)
+            .query(&[("keywords", &name)])
+            .send()
+            .await?
+            .text()
+            .await?;
+        let html = Html::parse_document(&text);
+
+        let Some(found) = html.select(&self.selectors.item).find(|item| {
+            item.select(&self.selectors.title)
+                .next()
+                .map(|title| title.text().collect::<String>())
+                .map(|title| title == name)
+                .unwrap_or(false)
+        }) else {
+            bail!("item not found");
+        };
+
+        if let Some(title) = found
+            .select(&self.selectors.title)
+            .next()
+            .map(|title| title.text().collect::<String>())
+        {
+            nfo.set_title(title);
+        }
+
+        if let Some(author) = found
+            .select(&self.selectors.author)
+            .next()
+            .map(|author| author.text().collect::<String>())
+        {
+            nfo.set_director(author);
+        }
+
+        if let Some(date) = found
+            .select(&self.selectors.date)
+            .next()
+            .map(|date| date.text().collect::<String>())
+            .and_then(|date| {
+                date.split_once('|')
+                    .map(|(date, _)| date.trim().to_string())
+            })
+        {
+            nfo.set_premiered(date);
+        }
+
+        if let Some(runtime) =
+            found
+                .select(&self.selectors.runtime)
+                .next()
+                .map(|runtime| runtime.text().collect::<String>())
+                .map(|runtime| {
+                    runtime.trim().split(':').take(2).enumerate().fold(
+                        0,
+                        |mut runtime, (idx, num)| {
+                            let num = num.parse().unwrap_or(0);
+
+                            match idx {
+                                0 => {
+                                    runtime += num * 60;
+                                }
+                                1 => {
+                                    runtime += num;
+                                }
+                                _ => {}
+                            }
+
+                            runtime
+                        },
+                    )
+                })
+        {
+            nfo.set_runtime(runtime);
+        }
+
+        found
+            .select(&self.selectors.fanart)
+            .next()
+            .and_then(|img| img.attr("style"))
+            .and_then(|sty| sty.split("'").nth(1).map(|fanart| fanart.to_string()))
+            .ok_or(anyhow!("fanart not found"))
     }
 }
