50b030e9a7fdc1b9726e034cc3a4441c870c70f7
jxt <jjt1525@outlook.com>
2025-03-07 00:43:15 +0800

    fix terminal size

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -4,8 +4,8 @@ pub const NAME: &str = "javcap";
 pub const VERSION: &str = env!("VERSION");
 pub const HASH: &str = env!("HASH");
 pub static LINE_LENGTH: LazyLock<usize> = LazyLock::new(|| {
-    termion::terminal_size()
-        .map(|(width, _)| width as usize)
+    terminal_size::terminal_size()
+        .map(|(width, _)| width.0 as usize)
         .unwrap_or(40)
 });
 pub const USER_AGENT: &str = concat!("javcap", "/", env!("VERSION"));
